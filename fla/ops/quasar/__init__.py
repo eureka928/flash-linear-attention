3cd16ff68f0964b9aa6dc2744945a6ed787559d7
quasar-miner <miner@quasar.subnet>
2026-03-13 23:44:13 +0000

    quasar: BYOC optimized kernel

--- fla/ops/quasar/__init__.py
+++ fla/ops/quasar/__init__.py
@@ -1,7 +1,13 @@
 from .chunk import chunk_quasar
 from .fused_recurrent import fused_recurrent_quasar
 
+# Preserve the upstream gated-delta-product API names expected by FLA.
+chunk_gated_delta_product = chunk_quasar
+fused_recurrent_gated_delta_product = fused_recurrent_quasar
+
 __all__ = [
+    "chunk_gated_delta_product",
     "chunk_quasar",
-    "fused_recurrent_quasar"
+    "fused_recurrent_gated_delta_product",
+    "fused_recurrent_quasar",
 ]
